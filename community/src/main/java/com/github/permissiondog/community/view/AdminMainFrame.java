afcffbf3f5a167b3d6838bca1e7e45882dca4947
PermissionDog <455662503@qq.com>
2022-05-25 21:36:19 +0800

    管理员窗口

--- community/src/main/java/com/github/permissiondog/community/view/AdminMainFrame.java
+++ community/src/main/java/com/github/permissiondog/community/view/AdminMainFrame.java
@@ -13,12 +13,16 @@ import java.util.List;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollPane;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class AdminMainFrame extends MainFrame {
 	private static final long serialVersionUID = 1L;
 	private JTable table;
 
 	private List<User> users;
+	private JTextField textFieldSearch;
 
 	public AdminMainFrame(User user, List<User> users) {
 		super(user);
@@ -26,21 +30,57 @@ public class AdminMainFrame extends MainFrame {
 
 		setTitle("管理员: " + user.getName());
 
-		setBounds(100, 100, 600, 400);
+		setBounds(100, 100, 705, 422);
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 0, 480, 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 0, 240, 0, 0 };
-		gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 1.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.columnWidths = new int[] { 0, 600, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 40, 80, 220, 40, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, 0.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		getContentPane().setLayout(gridBagLayout);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 1;
+		getContentPane().add(panel, gbc_panel);
+		
+		JButton btnNewUser = new JButton("新增用户");
+		btnNewUser.setBounds(10, 10, 93, 23);
+		panel.add(btnNewUser);
+		
+		JButton btnDeleteUser = new JButton("删除用户");
+		btnDeleteUser.setBounds(10, 42, 93, 23);
+		panel.add(btnDeleteUser);
+		
+		textFieldSearch = new JTextField();
+		textFieldSearch.setToolTipText("输入关键词搜索");
+		textFieldSearch.setBounds(160, 42, 201, 23);
+		panel.add(textFieldSearch);
+		textFieldSearch.setColumns(10);
+		
+		JButton btnNewMember = new JButton("添加老人");
+		btnNewMember.setBounds(492, 10, 93, 23);
+		panel.add(btnNewMember);
+		
+		JButton btnSetService = new JButton("设置服务");
+		btnSetService.setBounds(492, 42, 93, 23);
+		panel.add(btnSetService);
+		
+		JButton btnShowService = new JButton("查看服务");
+		btnShowService.setBounds(389, 42, 93, 23);
+		panel.add(btnShowService);
 
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 1;
-		gbc_scrollPane.gridy = 1;
+		gbc_scrollPane.gridy = 2;
 		getContentPane().add(scrollPane, gbc_scrollPane);
 
 		table = new JTable();
@@ -61,6 +101,8 @@ public class AdminMainFrame extends MainFrame {
 						"\u7535\u8BDD", "\u6743\u9650" }));
 		scrollPane.setViewportView(table);
 		flushTable();
+		
+		
 	}
 
 	private void flushTable() {
@@ -102,5 +144,4 @@ public class AdminMainFrame extends MainFrame {
 			}
 		});
 	}
-
 }
