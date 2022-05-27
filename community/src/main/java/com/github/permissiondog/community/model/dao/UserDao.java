bf0e85c936144bd77580d3fffc17285cbde1b88f
PermissionDog <455662503@qq.com>
2022-05-27 09:47:42 +0800

    UserDao

--- community/src/main/java/com/github/permissiondog/community/model/dao/UserDao.java
+++ community/src/main/java/com/github/permissiondog/community/model/dao/UserDao.java
@@ -5,6 +5,38 @@ import java.util.List;
 import com.github.permissiondog.community.model.User;
 
 public interface UserDao {
+	/**
+	 * 查找用户
+	 *
+	 * @param	username 用户名
+	 * @return	找到的用户, 如果未找到返回 null
+	 */
 	public User findUserByUserName(String username);
+	/**
+	 * 获取所有用户
+	 *
+	 * @return	所有用户
+	 */
 	public List<User> getAllUsers();
+	/**
+	 * 新增用户
+	 * 
+	 * @param	user 要插入的用户
+	 * @return	插入成功返回新用户, 否则返回 null
+	 */
+	public User insertUser(User user);
+	/**
+	 * 删除用户
+	 * 
+	 * @param	id 用户ID
+	 * @return 	删除成功返回被删除的用户, 否则返回 null
+	 */
+	public User deleteUser(int id);
+	/**
+	 * 修改用户
+	 * 
+	 * @param user	要更新的用户
+	 * @return		成功返回更新后的用户, 否则返回 null
+	 */
+	public User updateUser(User user);
 }
--- community/src/main/java/com/github/permissiondog/community/model/dao/impl/UserDaoImpl.java
+++ community/src/main/java/com/github/permissiondog/community/model/dao/impl/UserDaoImpl.java
@@ -11,28 +11,38 @@ import com.google.gson.reflect.TypeToken;
 
 public class UserDaoImpl implements UserDao {
 	private static UserDao userDao;
+
 	public static UserDao getInstance() {
 		if (userDao == null) {
 			userDao = new UserDaoImpl();
 		}
 		return userDao;
 	}
+
 	private UserDaoImpl() {
 		loadUsers();
 	};
-	
+
 	private Map<Integer, User> users;
 	private int idCount;
+
+	/**
+	 * 从内存中加载用户数据
+	 */
 	private void loadUsers() {
 		users = new HashMap<>();
 		String userJson = FileUtil.readFile(Constants.USER_TABLE_NAME);
-		Table<User> userTable = GsonUtil.gson.fromJson(userJson, 
-				new TypeToken<Table<User>>() {}.getType());
+		Table<User> userTable = GsonUtil.gson.fromJson(userJson, new TypeToken<Table<User>>() {
+		}.getType());
 		idCount = userTable.getIdCount();
 		for (User u : userTable.getData()) {
 			users.put(u.getId(), u);
 		}
 	}
+
+	/**
+	 * 将内存中的用户数据保存至文件
+	 */
 	private void saveUsers() {
 		Table<User> userTable = new Table<>();
 		userTable.setIdCount(idCount);
@@ -40,13 +50,8 @@ public class UserDaoImpl implements UserDao {
 		String userJson = GsonUtil.gson.toJson(userTable);
 		FileUtil.writeFile(Constants.USER_TABLE_NAME, userJson);
 	}
+
 	
-	/**
-	 *	查找用户
-	 *
-	 *	@param username 用户名
-	 *	@return 找到的用户, 如果未找到返回 null
-	 */
 	@Override
 	public User findUserByUserName(String username) {
 		for (User u : users.values()) {
@@ -56,15 +61,39 @@ public class UserDaoImpl implements UserDao {
 		}
 		return null;
 	}
-	
-	/**
-	 *	获取所有用户
-	 *
-	 *	@return 所有用户
-	 */
+
+
 	@Override
 	public List<User> getAllUsers() {
 		return new ArrayList<>(users.values());
 	}
-	
+
+	@Override
+	public User insertUser(User user) {
+		user.setId(idCount++);
+		users.put(user.getId(), user);
+		saveUsers();
+
+		return user;
+	}
+
+	@Override
+	public User deleteUser(int id) {
+		User u = users.remove(id);
+		if (u != null)
+			saveUsers();
+
+		return u;
+	}
+
+	@Override
+	public User updateUser(User user) {
+		if (!users.containsKey(user.getId())) {
+			return null;
+		}
+		users.put(user.getId(), user);
+		saveUsers();
+		return user;
+	}
+
 }
